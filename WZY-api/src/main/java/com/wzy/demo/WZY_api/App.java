34c69befaccdcbd232c367e204b8c32f8953fed2
zheyi <chriswangzheyi@hotmail.com>
2018-02-08 14:51:25 +0800

    Initial commit

---
+++ WZY-api/src/main/java/com/wzy/demo/WZY_api/App.java
@@ -0,0 +1,13 @@
+package com.wzy.demo.WZY_api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
